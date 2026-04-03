6aa952424852bed2a1ab3547edee7599fb943fe3
Pranav Saravanan <pranavss722@gmail.com>
2026-04-02 21:44:56 -0700

    Revert "feat: initial rayserve ad scoring pipeline"
    This reverts commit 3fbcf08c8ef85f72abf672c4492d47761a4a460a.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
-import os
-
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="RAG Pipeline", version="0.1.0")
 
-if os.path.exists("app/static"):
-    app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
 @app.get("/")
